2b6152fd4ee43281911a04123d7f8e3118b1721d
paularinzee <arinzennajipual@gmail.com>
2024-12-19 18:15:45 +0100

    ....

--- front-end-nextjs/src/app/api/generate-qr/route.js
+++ front-end-nextjs/src/app/api/generate-qr/route.js
@@ -10,8 +10,8 @@ export async function POST(request) {
   }
 
   try {
-    const response = await axios.post(`http://qr-api-service/generate-qr/?url=${encodeURIComponent(url)}`);
-    // const response = await axios.post(`http://localhost:8000/generate-qr/?url=${url}`);
+    // const response = await axios.post(`http://qr-api-service/generate-qr/?url=${encodeURIComponent(url)}`);
+    const response = await axios.post(`http://localhost:8000/generate-qr/?url=${url}`);
 
     return NextResponse.json(response.data);
   } catch (error) {

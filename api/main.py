abaf380caec6198242dab650ad01be2077b2a5eb
paularinzee <arinzennajipual@gmail.com>
2024-12-23 13:42:44 +0100

    ...,.

--- api/main.py
+++ api/main.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Set Google Application Credentials from .env
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+# os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
 
 app = FastAPI()
 

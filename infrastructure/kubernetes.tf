f887366ecdd4b0f3cca835920a55c835e60582a3
paularinzee <arinzennajipual@gmail.com>
2025-01-22 16:32:45 +0100

    add k8s secret

--- infrastructure/kubernetes.tf
+++ infrastructure/kubernetes.tf
@@ -1,44 +1,40 @@
-resource "google_container_cluster" "primary" {
-  name = "primary"
-  location = "us-central1-a"
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/container_cluster
+resource "google_container_cluster" "example-cluster" {
+  name                     = var.cluster_name
+  location                 = var.zone
   remove_default_node_pool = true
-  initial_node_count = 1
-  network = google_compute_network.main-vpc.self_link
-  subnetwork = google_compute_subnetwork.private.self_link
-  logging_service = "logging.googleapis.com/kubernetes"
-  monitoring_service = "monitoring.googleapis.com/kubernetes"
+  initial_node_count       = 1
+  network                  = google_compute_network.main.self_link
+  subnetwork               = google_compute_subnetwork.private.self_link
+  logging_service          = "none"
+  monitoring_service       = "none"
+  networking_mode          = "VPC_NATIVE"
 
-  node_locations = [ 
-    "us-central1-b"
-   ]
+  addons_config {
+    http_load_balancing {
+      disabled = true
+    }
+    horizontal_pod_autoscaling {
+      disabled = false
+    }
+  }
 
-   addons_config {
-     http_load_balancing {
-       disabled = true
-     }
-     horizontal_pod_autoscaling {
-       disabled = false
-     }
-   }
-   release_channel {
-     channel = "REGULAR"
-   }
-   workload_identity_config {
-     workload_pool = "paul-devops.svc.id.goog"
-   }
+  release_channel {
+    channel = "REGULAR"
+  }
 
-   ip_allocation_policy {
-     cluster_secondary_range_name = "k8s-pod-range"
-     services_secondary_range_name = "k8s-service-range"
-   }
+  workload_identity_config {
+    workload_pool = "${var.project_id}.svc.id.goog"
+  }
 
-   private_cluster_config {
-     enable_private_nodes = true
-     enable_private_endpoint = false
-     master_ipv4_cidr_block = "172.16.0.0/28"
-   }
+  ip_allocation_policy {
+    cluster_secondary_range_name  = "k8s-pods-ip-range"
+    services_secondary_range_name = "k8s-services-ip-range"
+  }
 
-
-
-
+  private_cluster_config {
+    enable_private_nodes    = true
+    enable_private_endpoint = false
+    master_ipv4_cidr_block  = "172.16.0.0/28"
+  }
 }
